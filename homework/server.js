1166bbbae444bcee61c94e4078bffb19edef0c32
Bogdan Birau <birau.bogdan92@gmail.com>
2021-11-30 09:07:07 +0100

    updated

--- homework/server.js
+++ homework/server.js
@@ -1,18 +1,19 @@
-import express from "express"
-import listEndpoints from "express-list-endpoints"
-import authorsRouter from "./authors/index.js"
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import authorsRouter from "./authors/index.js";
+import cors from "cors";
 
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 
-server.use("/authors", authorsRouter)
+server.use("/authors", authorsRouter);
 
-const port = 3001
+const port = 3001;
 
-console.table(listEndpoints(server))
+console.table(listEndpoints(server));
 
 server.listen(port, () => {
-    console.log("Serveris running on port:", port)
-})
-
+  console.log("Serveris running on port:", port);
+});
